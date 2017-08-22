ed3e5b22305821f02ccc9362527f9e3104de1adb
Michael Petretti <michael.petretti@skyscanner.net>
2017-08-22 14:10:38 +0200

    Add twitter streaming api example

---
+++ twitter_code/streaming_example.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+# Copyright 2007-2016 The Python-Twitter Developers
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import json
+
+from twitter import Api
+
+CONSUMER_KEY = os.getenv("CONSUMER_KEY", None)
+CONSUMER_SECRET = os.getenv("CONSUMER_SECRET", None)
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN", None)
+ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET", None)
+
+# Users to watch
+USERS = ['25073877'] # This is Donald Trump
+
+# Languages to filter tweets by is a list. This will be joined by Twitter
+# to return data mentioning tweets only in the english language.
+LANGUAGES = ['en']
+
+# Since we're going to be using a streaming endpoint, there is no need to worry
+# about rate limits.
+api = Api(CONSUMER_KEY,
+          CONSUMER_SECRET,
+          ACCESS_TOKEN,
+          ACCESS_TOKEN_SECRET)
+
+
+def main():
+    with open('output.txt', 'a') as f:
+        # api.GetStreamFilter will return a generator that yields one status
+        # message (i.e., Tweet) at a time as a JSON dictionary.
+        for line in api.GetStreamFilter(follow=USERS, languages=LANGUAGES):
+            # we don't want answers and retweets of other users
+            if line['user']['id'] == 25073877:
+                print(json.dumps(line))
+                f.write(json.dumps(line))
+                f.write('\n')
+
+
+if __name__ == '__main__':
+    main()
